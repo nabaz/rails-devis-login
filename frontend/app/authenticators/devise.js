990c81bfd0f38262352c5b32267265dbd669c679
nmaaruf <nabaz.maaruf@aenetworks.com>
2016-08-31 22:27:42 -0400

    added frontend

---
+++ frontend/app/authenticators/devise.js
@@ -0,0 +1,6 @@
+// app/authenticators/devise.js
+import Devise from 'ember-simple-auth/authenticators/devise';
+
+export default Devise.extend({
+  serverTokenEndpoint: 'http://localhost:3000/users/sign_in'
+});
